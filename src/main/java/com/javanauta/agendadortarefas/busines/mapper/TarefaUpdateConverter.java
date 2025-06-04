9cbabb5c75467c9b3acaf9785c9e2cf243b1e0e2
dev-LierBag <gabrielalves.negocios@gmail.com>
2025-06-04 18:02:37 -0300

    adicionado dockerfile e alterações necessarias

--- src/main/java/com/javanauta/agendadortarefas/busines/mapper/TarefaUpdateConverter.java
+++ src/main/java/com/javanauta/agendadortarefas/busines/mapper/TarefaUpdateConverter.java
@@ -1,6 +1,6 @@
 package com.javanauta.agendadortarefas.busines.mapper;
 
-import com.javanauta.agendadortarefas.busines.dto.TarefasDTO;
+import com.javanauta.agendadortarefas.busines.dto.TarefasDTORecords;
 import com.javanauta.agendadortarefas.infrastructure.entity.TarefasEntity;
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingTarget;
@@ -9,5 +9,5 @@ import org.mapstruct.NullValuePropertyMappingStrategy;
 @Mapper(componentModel = "spring", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
 public interface TarefaUpdateConverter {
 
-    void updateTarefas(TarefasDTO dto, @MappingTarget TarefasEntity entity);
+    void updateTarefas(TarefasDTORecords dto, @MappingTarget TarefasEntity entity);
 }
